94886426d78bdfbdfe271fb336369a8ab2a1ddcd
Max0nyshchenko <max.0nyshenk03@gmail.com>
2020-04-23 18:57:27 +0300

    finished responsivennes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 const bottom_container = document.querySelector('.bottom-container');
+const icons = document.querySelectorAll('.special');
 
 // set initial state of menu
 let showMenu = false;
@@ -35,4 +36,10 @@ function toggleMenu() {
 
 document.addEventListener('scroll', () => {
   bottom_container.style.visibility = 'visible';
+  bottom_container.style.opacity = '1';
+  bottom_container.style.transform = 'translate(0,0)';
+});
+
+icons.forEach((item) => {
+  item.style.transition = 'none';
 });
